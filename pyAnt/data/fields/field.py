b7a140a4b9574d0fca7ce0285251e33a0eef5ce4
Tao Ji <taoji.cs@gmail.com>
2018-11-27 22:29:34 +0800

    init(TokenIndexer): Define the TokenIndexer class.

--- pyAnt/data/fields/field.py
+++ pyAnt/data/fields/field.py
@@ -7,13 +7,26 @@ class Field(metaclass=ABCmeta):
     A ``Field`` is an ingredient of a data instance. In most NLP tasks, ``Field``
     stores data of string types. It contains one or more indexers that map string
     data to the corresponding index. Data instances are collections of fields.
-
-    For example,
     """
     @abstractmethod
     def count_vocab_items(self, counter: Dict[str, Dict[str, int]]):
+        """
+        We count the number of strings if the string needs to be mapped to one
+        or more integers. You can pass directly if there is no string that needs
+        to be mapped.
+
+        Parameters
+        ----------
+        counter : ``Dict[str, Dict[str, int]]``
+        ``counter`` is used to count the number of each item. The first key
+        represents the namespace of the vocabulary, and the second key represents
+        the string of the item.
+        """
         pass
 
     @abstractmethod
     def index(self, vocab: Vocabulary):
+        """
+        G
+        """
         pass
---
+++ pyAnt/data/token_indexers/__init__.py
@@ -0,0 +1,3 @@
+from typing import List, Dict, TypeVar, Generic
+
+IndexT
