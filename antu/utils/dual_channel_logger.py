91820c887047f0a8772163dfec8fd156976deb82
Tao Ji <taoji.cs@gmail.com>
2019-01-22 16:55:33 +0800

    features(utils): Optimize the dual_channel_logger module.

--- antu/utils/dual_channel_logger.py
+++ antu/utils/dual_channel_logger.py
@@ -8,18 +8,19 @@ def dual_channel_logger(
     file_model: str='w+',
     file_level: int=logging.DEBUG,
     console_level: int=logging.DEBUG,
-    formatter: str='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    formatter: str='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    time_formatter: str='%y-%m-%d %H:%M:%S'
     ) -> logging.Logger:
 
     logger = logging.getLogger(name)
-    logger.setLevel(logging.ERROR)
-    formatter = logging.Formatter(formatter)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(formatter, time_formatter)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(console_level)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
     if file_path:
-        file_handler = logging.FileHandler(file_path, model)
+        file_handler = logging.FileHandler(file_path, file_model)
         file_handler.setLevel(file_level)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
