3dc9db5b207202ed8eb4b5348cac4dfe8fe9c8bd
Tao Ji <jitao.1995@outlook.com>
2019-07-18 13:26:16 +0800

    refactor(antu.nn): Refactor MLP modules.
    '
    q
    
    q
    '

--- antu/nn/dynet/modules/perceptron.py
+++ antu/nn/dynet/modules/perceptron.py
@@ -1,54 +1,45 @@
 import dynet as dy
 from antu.nn.dynet.initializer import orthonormal_initializer
+from . import dy_model
 
 
+@dy_model
 class MLP(object):
     """docstring for MLP"""
 
     def __init__(
             self,
-            model,
-            sizes, act=dy.tanh, init=0, bias=True, dropout=0.0):
+            model: dy.ParameterCollection,
+            sizes: List[int],
+            f: 'nonlinear' = dy.tanh, 
+            p: float = 0.0,
+            bias: bool = True, 
+            init: dy.PyInitializer = dy.GlorotInitializer()):
+        
         pc = model.add_subcollection()
-        if init != 'orthonormal':
-            self.W = [
-                pc.add_parameters((x, y), init)
-                for x, y in zip(sizes[1:], sizes[:-1])]
-        else:
-            self.W = [
-                pc.parameters_from_numpy(orthonormal_initializer(x, y))
-                for x, y in zip(sizes[1:], sizes[:-1])]
+        self.W = [
+            pc.add_parameters((x, y), init)
+            for x, y in zip(sizes[1:], sizes[:-1])]
+        '''
+        self.W = [
+            pc.parameters_from_numpy(orthonormal_initializer(x, y))
+            for x, y in zip(sizes[1:], sizes[:-1])]
+        '''
         if bias:
             self.b = [pc.add_parameters((y,), init=0) for y in sizes[1:]]
 
-        self.pc, self.act, self.bias = pc, act, bias
-        self.dropout = dropout
-        self.spec = (sizes, act, init, bias, dropout)
+        self.pc, self.f, self.p, self.bias = pc, f, p, bias
+        self.spec = (sizes, f, p, bias, init)
 
     def __call__(self, x, train=False):
         h = x
         # for W, b in zip(self.W[:-1], self.b[:-1]):
         for i in range(len(self.W[:-1])):
-            h = self.act(self.W[i]*h + (self.b[i] if self.bias else 0))
+            h = self.f(self.W[i]*h + (self.b[i] if self.bias else 0))
             if train:
                 if len(h.dim()[0]) > 1:
-                    h = dy.dropout_dim(h, 1, self.dropout)
+                    h = dy.dropout_dim(h, 1, self.p)
                 else:
-                    h = dy.dropout(h, self.dropout)
+                    h = dy.dropout(h, self.p)
         return self.W[-1]*h + (self.b[-1] if self.bias else 0)
 
-    @staticmethod
-    def from_spec(spec, model):
-        """Create and return a new instane with the needed parameters.
-
-        It is one of the prerequisites for Dynet save/load method.
-        """
-        sizes, act, init, bias, dropout = spec
-        return MLP(model, sizes, act, init, bias, dropout)
-
-    def param_collection(self):
-        """Return a :code:`dynet.ParameterCollection` object with the parameters.
-
-        It is one of the prerequisites for Dynet save/load method.
-        """
-        return self.pc
